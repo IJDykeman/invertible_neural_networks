f1d5529682e4c28c2b9fc881cc0abc08499d44e2
idykeman <idykeman@uber.com>
2018-08-03 17:18:48 -0400

    add rotator class

--- inet.py
+++ inet.py
@@ -12,22 +12,25 @@ def split(x):
 def fuse(x1, x2):
     return tf.concat([x1, x2], -1)
 
-def rotate(x):
-    """
-    This method could be replaced with shuffling (as in Real NVP) or with a learned rotation, as in GLOW.
-    This rotation by reversal of feature order is used in NICE
-    """
-    return x[:,::-1]
 
-def rotate_inverse(x):
-    return x[:,::-1]
+class HalfChannelsRotator:
+    def rotate(self, x):
+        """
+        This method could be replaced with shuffling (as in Real NVP) or with a learned rotation, as in GLOW.
+        This rotation by reversal of feature order is used in NICE
+        """
+        return x[:,::-1]
+
+    def rotate_inverse(self, x):
+        return x[:,::-1]
 
 class DenseLayer:
-    def __init__(self, x, unique_name=""):
+    def __init__(self, x, rotator, unique_name=""):
         # we need to ensure that someone does not accidentalyl end up with 2 layers with the same name
+        self.rotator = rotator
         self.unique_name = unique_name + uuid.uuid4().hex.upper()[0:6] 
         half_x_dim = x.get_shape()[1].value // 2
-        x_rotated = rotate(x)
+        x_rotated = self.rotator.rotate(x)
         x1, x2 = split(x_rotated)
         scale, translate = self.get_transform(x1)
         x2_transformed = (x2 * scale) + translate
@@ -51,12 +54,12 @@ class DenseLayer:
         scale, translate = self.get_transform(x1)
         x2 = (x2_transformed - translate) / scale
         x_rotated = tf.concat([x1, x2], -1)
-        x = rotate_inverse(x_rotated)
+        x = self.rotator.rotate_inverse(x_rotated)
         self.reverse_output = x
 
 for i in range(1000):
     x = tf.constant([[1.0, 2.0, 3.0, 4.0, 5.0, 6.0]])
-    layer = DenseLayer(x, unique_name="layer1")
+    layer = DenseLayer(x, HalfChannelsRotator(), unique_name="layer1")
     y = layer.forward_output
     layer.reverse(y)
     x_recovered = layer.reverse_output
